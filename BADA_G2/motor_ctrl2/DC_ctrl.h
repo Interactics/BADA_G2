684e0b2b926a5c0e7821f81e4545b38c4b928f7c
Interactics <zz31@naver.com>
2020-07-23 16:39:57 +0900

    Update DC_ctrl.cpp & .h

--- BADA_G2/motor_ctrl2/DC_ctrl.h
+++ BADA_G2/motor_ctrl2/DC_ctrl.h
@@ -2,7 +2,7 @@
 #ifndef DC_CTRL_H_
 #define DC_CTRL_H_
 
-enum M_Locate{LEFT, RIGHT};   //Robot's Location of Motor; LEFT or RIGHT? 
+enum M_Locate{LEFT, RIGHT};             //Robot's Location of Motor; LEFT or RIGHT? 
 
 const byte R_MOTOR_ENCOD_A = 12;
 const byte R_MOTOR_ENCOD_B = 11;
@@ -14,9 +14,10 @@ const byte L_MOTOR_ENCOD_B = 9;
 const byte L_MOTOR_PWM   = 5;
 const byte L_MOTOR_DIR   = 3;
 
-const int WHEELSIZE     = 84;  //Wheel Size
-const int WHEELBASE     = 999;
-const int ENCOD_RESOL   = 1612 // Pulse Per Round (31gear * 13)402 Pulse/CH x 4 
+const int WHEELSIZE         = 84;       // Wheel Size
+const int WHEELBASE         = 999;      // Wheel to wheel distance
+const int ENCOD_RESOL       = 1612      // Pulse Per Round (31gear * 13)402 Pulse/CH x 4 
+const int CONTROL_FREQUENCY = 20        // [ms]
 
 const double PI         = 3.141592;
 
@@ -44,36 +45,41 @@ private:
     int    Position_current_;
     int    Position_diff_;
 
-    float  err_instant_;
+    float  err_prev_;
     float  err_sum_;
-    double Vel_Target_;
+    double Spd_target_;
+
+    double Velocity_; //current speed
 
     void callbackEncod_A_();
     void callbackEncod_B_();
 
+    void WritePWM_(bool Dir, int PWM);  // Writing PWM on PWN Pin.
+
+
+    double Trans_Spd2Encod_(double spd);   // Transforming Speed to encoder val 
+    double Trans_Encod2Spd_(double encod); // Transforming Encoder val to Speed
+
 public:
     bool Direction_current;
     int  PWM_current;
-    double Velocity;
     
     DCMotor(int pin_encod_A, int pin_encod_B, int pin_direct, int pin_PWM, M_Locate M_LOC);
     ~DCMotor();
 
     void PIDgainSet(float P_gain, float I_gain, float D_gain);
-    int EncoderPos();                  // Returning current encoder's position.
-    void WritePWM(bool Dir, int PWM);  // Writing PWM on PWN Pin.
-    void CalcEncodDiff();              // Calulating Diff of Encoder.
+    int EncoderPos();                     // Returning current encoder's position.
+    void CalcEncodDiff();                 // Calulating Diff of Encoder.
 
-    void MotorControl(float TargetSpd); // Controlling MotorSpd using PID_Ctrl.
-    void SetUpSpd(double TargetSpd);    // Setting up Target Speed.
+    void MotorControl(float TargetSpd);   // Controlling MotorSpd using PID_Ctrl.
+    void SetUpSpd(double TargetSpd);      // Setting up Target Speed.
 
-    double Trans_Spd2Encod(double spd); // Transforming Speed to encoder val 
-    double Trans_Spd2Vel(double encod); // Transforming Encoder val to Speed
+    // double Trans_Spd2Encod(double spd);   // Transforming Speed to encoder val 
+    // double Trans_Encod2Spd(double encod); // Transforming Encoder val to Speed
 
     void EncodDiff();
     void CalcSpd();
     void PID_Update();
-
 }
 
 
