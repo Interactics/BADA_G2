b114130bac6251a3d8ddf12909cd06c681108818
Interactics <zz31@naver.com>
2020-07-24 23:09:19 +0900

    update motor_control

--- BADA_G2/motor_control/DC_ctrl.cpp
+++ BADA_G2/motor_control/DC_ctrl.cpp
@@ -1,11 +1,20 @@
 // DC_ctrl.cpp
-#include <DC_ctrl.h>
+#include <Arduino.h>
+#include "include/DC_ctrl.h"
 
 /******************************************
 *          Method of DCMotor Class        *
 ******************************************/
 
-///---- TODO -- Callback of Encoder 
+/**********************
+        TODO LIST
+    1. Update Trans_Spd2Encod_
+    2. Update Trans_Encod2Spd_
+    3. Update Velocity_ = Trans_Encod2Spd_(CurrentEncod); // FIX THIS!!!!! Variable is wrong
+    4. Refactoring 
+    
+************************/
+
 
 DCMotor::DCMotor(int pin_encod_A, int pin_encod_B, int pin_direct, int pin_PWM, M_Locate M_LOC)
     :MOTR_ENCOD_A_(pin_encod_A), MOTR_ENCOD_B_(pin_encod_B), 
@@ -14,14 +23,17 @@ DCMotor::DCMotor(int pin_encod_A, int pin_encod_B, int pin_direct, int pin_PWM, 
     // Encoder pin Setup
     pinMode(pin_encod_A, INPUT_PULLUP);
     pinMode(pin_encod_B, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(pin_encod_A), this->callbackEncod_A, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(pin_encod_B), this->callbackEncod_B, CHANGE);
+    
+    ////////////////// NOT WORK //////////////////////
+    // attachInterrupt(digitalPinToInterrupt(pin_encod_A), reinterpret_cast<void (*)()>(&callbackEncod_A_), CHANGE);
+    // attachInterrupt(digitalPinToInterrupt(pin_encod_B), reinterpret_cast<void (*)()>(&callbackEncod_B_), CHANGE);
 
     // PID Error Setup
-    err_instant_      = 0.0f;
+    err_prev_         = 0.0f;
     err_sum_          = 0.0f;
 
     // Encoder Position Setup
+    Encoder_          = 0;
     Position_prev_    = 0;
     Position_current_ = 0;
     Position_diff_    = 0;
@@ -29,6 +41,10 @@ DCMotor::DCMotor(int pin_encod_A, int pin_encod_B, int pin_direct, int pin_PWM, 
     // Other Motor Setup
     PWM_current_      = 0;
     Direction_current = true; 
+}
+
+DCMotor::~DCMotor(){
+
 }
 
 void DCMotor::PIDgainSet(float P_gain, float I_gain, float D_gain){
@@ -49,15 +65,15 @@ void DCMotor::WritePWM_(bool Dir, int PWM){
  
 void DCMotor::MotorControl(float TargetSpd){
     float Speed = Velocity_; // Current Speed 
-    float err = 0f;
-    float K_P = 0f, 
-          K_I = 0f, 
-          K_D = 0f;
+    float err = 0.0f;
+    float K_P = 0.0f, 
+          K_I = 0.0f, 
+          K_D = 0.0f;
 
     int  u_val = 0;
     bool m_dir = false;
 
-    long double Y_output = 0f;
+    long double Y_output = 0.0f;
     long double TargetEncod;
     long double CurrentEncod;
     //long double inputSpeed;
@@ -69,11 +85,11 @@ void DCMotor::MotorControl(float TargetSpd){
     err_sum_ += err;
 
     K_P = P_gain_ * err;
-    K_I = I_gain_ * err_sum;
-    K_D = D_gain_ * (err - err_prev);
+    K_I = I_gain_ * err_sum_;
+    K_D = D_gain_ * (err - err_prev_);
 
     err_prev_ = err;
-    Y_output  = K_P + K_I _+ K_Dl
+    Y_output  = K_P + K_I + K_D;
 
     if(Y_output < 0){
         m_dir = false; 
@@ -84,7 +100,7 @@ void DCMotor::MotorControl(float TargetSpd){
 
     if (Y_output > 255) Y_output = 255;
     
-    Velocity_ = Trans_Spd2Vel
+    Velocity_ = Trans_Encod2Spd_(CurrentEncod); // FIX THIS!!!!! Variable is wrong
     
     WritePWM_(m_dir, Y_output);
 } // Controlling MotorSpd using PID_Ctrl.
@@ -104,9 +120,8 @@ double DCMotor::Trans_Spd2Encod_(double spd){
 double DCMotor::Trans_Encod2Spd_(double encod){
     // RPM = [del(encoder)/ms] * [1000ms/1sec] * [60sec/1 min] * [1round/1roundEncoder]
     // Velocity = [(RPM)Round/1min] * [1min/60sec] * [2pi*r/1Round] * [1m/1000sec]
-    
     double temp_RPM;
-    temp_RPM = (double(diff_Enc) / CONTROL_FREQUENCY) * (60 / 1) * (1000 / 1) * (1 / ENCODER_RESOLUTION); 
+    temp_RPM = (double(Position_diff_) / CONTROL_FREQUENCY) * (60 / 1) * (1000 / 1) * (1 / ENCODER_RESOLUTION); 
     return temp_RPM / 1 * 1 / 60 * 2*WHEELSIZE*PI / 1 * 1 / 1000; // Velocity
 } // Transforming Encoder val to Speed
 /********************************************************************************************/
@@ -117,12 +132,23 @@ void DCMotor::EncodDiff(){
 }
 
 void DCMotor::CalcSpd(){
-    Velocity_ = Trans_Encod2Spd(Position_diff_);
+    Velocity_ = Trans_Encod2Spd_(Position_diff_);
 }
 
 void DCMotor::PID_Update(){
     MotorControl(Spd_target_);
 }
 
-void DCMotor::callbackEncod_A_();
-void DCMotor::callbackEncod_B_();
+void DCMotor::callbackEncod_A(){
+    if (digitalRead(MOTR_ENCOD_A_) == digitalRead(MOTR_ENCOD_B_)) Encoder_++;
+    else Encoder_--;
+}
+void DCMotor::callbackEncod_B(){
+    if (digitalRead(MOTR_ENCOD_A_) == digitalRead(MOTR_ENCOD_B_)) Encoder_--;
+    else Encoder_++;
+}
+
+long DCMotor::ShowEncoder(){
+    return Encoder_;
+}
+
