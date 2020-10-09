bc3b968c96a68681317a7c21eef54613bad9f453
epikem <epikem1@gmail.com>
2020-10-09 20:37:54 +0900

    remove duplicate codes

--- BADA_G2_web/public/index.js
+++ BADA_G2_web/public/index.js
@@ -21,16 +21,22 @@ var data;
 var time;
 var sig_name;
 var viewtime;
+var hsignal;
+var signal;
+var audio_topic;
+var odom;
 var PrintHistory = "Connection";
 
 const today = new Date();
 today.setTime(0);
 
+const dic1 = { 'Cry': '아기 우는 소리', 'Alarm': '화재 경보', 'Door': '노크', 'Boiling': '물 끓는 소리', 'Silence': '조용한', 'Water': '물소리', 'Bell': '초인종 소리' };
+  
 const KAKAO_REST_API_TOKEN = "KAKAO_REST_API_TOKEN";
 const KAKAO_TOKEN = "KAKAO_TOKEN";
 
 Kakao.init(KAKAO_TOKEN);   // 사용할 앱의 JavaScript 키를 설정
-// init();
+init();
 /*  Kakao.Auth.createLoginButton({
     container: '#kakao-login-btn',
     size : 'small',
@@ -174,9 +180,6 @@ function printClock() {
   clock.innerHTML = currentHours + ":" + currentMinute + ":" + currentSeconds + " <span style='font-size:50px;'>" + amPm + "</span>";
   setTimeout("printClock()", 1000);
 
-
-
-
   function addZeros(num, digit) {
     var zero = '';
     num = num.toString();
@@ -241,160 +244,10 @@ function toString() {
   return retStr;
 }
 
-
-
 var water = new Queue();
 var h_element;
 var h = new Queue();
 
-
-
-var ros = new ROSLIB.Ros();
-
-// If there is an error on the backend, an 'error' emit will be emitted.
-ros.on('error', function (error) {
-  console.log(error);
-});
-
-// Find out exactly when we made a connection.
-ros.on('connection', function () {
-  console.log('Connection made!');
-});
-
-ros.on('close', function () {
-  console.log('Connection closed.');
-});
-
-// Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:9090');
-
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var listener = new ROSLIB.Topic({
-  ros: ros,
-  name: '/listener',
-  messageType: 'std_msgs/String'
-});
-
-// Then we add a callback to be called every time a message is published on this topic.
-listener.subscribe(function (message) {
-  // console.log('Received message on ' + listener.name + ': ' + message.data);
-
-  // If desired, we can unsubscribe from the topic as well.
-  listener.unsubscribe();
-});
-
-
-
-
-var hsignal = new ROSLIB.Topic({
-  ros: ros,
-  name: '/signal',
-  //name : '/bada_audio/signal',
-  messageType: 'std_msgs/String'
-});
-const dic1 = { 'Cry': '아기 우는 소리', 'Alarm': '화재 경보', 'Door': '노크', 'Boiling': '물 끓는 소리', 'Silence': '조용한', 'Water': '물소리', 'Bell': '초인종 소리' };
-
-hsignal.subscribe(function (m) {
-
-  sig_name = dic1[m.data];
-  console.log("NOW SIGNAL : " + sig_name);
-
-
-  time = today.getTime();
-  viewtime = printNow();
-
-  if (h.dataStore.length >= 17) {
-    h.dequeue();
-  }
-
-  if (sig_name == dic1['Water']) {
-    //먼저 검색해 
-    if (water.search()) {
-      //총 3번 이상 발생했다면
-      for (var i = 1; i <= 3; i++) {
-        water.dequeue();
-      }
-      cnt = 0;
-      shareKakaotalk(sig_name);
-      h.enqueue([sig_name, viewtime]);
-      hidx = hidx + 1;
-      PrintHistory = h.toString();
-    }
-    else {
-      //발생한적없다면
-      water.enqueue(time);
-    }
-  }
-  else if (sig_name != dic1["Silence"]) {
-
-    shareKakaotalk(sig_name);
-    h.enqueue([sig_name, viewtime]);
-    hidx = hidx + 1;
-    PrintHistory = h.toString();
-  }
-  document.getElementById("history").innerHTML = PrintHistory;
-
-});
-
-//ALARM Section
-
-// Connecting to ROS
-
-var ros = new ROSLIB.Ros();
-
-// If there is an error on the backend, an 'error' emit will be emitted.
-ros.on('error', function (error) {
-  console.log(error);
-});
-
-// Find out exactly when we made a connection.
-ros.on('connection', function () {
-  console.log('Connection made!');
-  init();
-});
-
-ros.on('close', function () {
-  console.log('Connection closed.');
-});
-
-// Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:9090');
-
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var listener = new ROSLIB.Topic({
-  ros: ros,
-  name: '/listener',
-  messageType: 'std_msgs/String'
-});
-
-// Then we add a callback to be called every time a message is published on this topic.
-listener.subscribe(function (message) {
-  // console.log('Received message on ' + listener.name + ': ' + message.data);
-
-  // If desired, we can unsubscribe from the topic as well.
-  listener.unsubscribe();
-});
-
-
-var ros = new ROSLIB.Ros({
-  url: 'ws://localhost:9090'
-});
-
-
-var signal = new ROSLIB.Topic({
-  ros: ros,
-  name: '/signal',
-  // name : '/bada_audio/signal',
-  messageType: 'std_msgs/String'
-});
-// const dic1={'Speech':'말하는 소리', 'Alarm':'화재 경보', 'Door':'노크', 'Television':'티비 소리', 'Silence':'조용해요', 'Water':'물소리', 'Music':'휴대폰 벨소리'};
-
-signal.subscribe(function (m) {
-  sig_name = dic1[m.data];
-  document.getElementById("signal").innerHTML = sig_name;
-});
 
 
 //from index.js
@@ -425,165 +278,6 @@ w_remove = setInterval(function () {
 var water = new Queue();
 
 
-////////////////function tryConnectWebsocket() {
-
-// Connecting to ROS
-// -----------------
-var ros = new ROSLIB.Ros();
-
-// If there is an error on the backend, an 'error' emit will be emitted.
-ros.on('error', function (error) {
-  // document.getElementById('connecting').style.display = 'none';
-  // document.getElementById('connected').style.display = 'none';
-  // document.getElementById('closed').style.display = 'none';
-  // document.getElementById('error').style.display = 'inline';
-  console.log(error);
-});
-
-// Find out exactly when we made a connection.
-ros.on('connection', function () {
-  console.log('Connection made!');
-  // document.getElementById('connecting').style.display = 'none';
-  // document.getElementById('error').style.display = 'none';
-  // document.getElementById('closed').style.display = 'none';
-  // document.getElementById('connected').style.display = 'inline';
-});
-
-ros.on('close', function () {
-  console.log('Connection closed.');
-  // document.getElementById('connecting').style.display = 'none';
-  // document.getElementById('connected').style.display = 'none';
-  // document.getElementById('closed').style.display = 'inline';
-});
-
-// Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:9090');
-
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var listener = new ROSLIB.Topic({
-  ros: ros,
-  name: '/listener',
-  messageType: 'std_msgs/String'
-});
-
-// Then we add a callback to be called every time a message is published on this topic.
-listener.subscribe(function (message) {
-  // console.log('Received message on ' + listener.name + ': ' + message.data);
-
-  // If desired, we can unsubscribe from the topic as well.
-  listener.unsubscribe();
-});
-
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var turtle1 = new ROSLIB.Topic({
-  ros: ros,
-  name: '/turtle1/pose',
-  messageType: 'turtlesim/Pose'
-});
-
-// Then we add a callback to be called every time a message is published on this topic.
-var count = 0;
-turtle1.subscribe(function (message) {
-  if (count) {
-    console.log(message);
-  }
-
-  // console.log('received a turtlesim message');
-  // console.log(message);
-  x = message.x * 50;
-  y = message.y * 50;
-  theta = message.theta;
-  // If desired, we can unsubscribe from the topic as well.
-  // if(count>5000){
-  //   turtle1.unsubscribe();
-  // }
-});
-
-
-var signal = new ROSLIB.Topic({
-  ros: ros,
-  name: '/signal',
-  messageType: 'std_msgs/String'
-});
-
-signal.subscribe(function (m) {
-  sig_name = m.data;
-
-
-  //console.log("NOW SIGNAL : "+sig_name);
-  document.getElementById("signal").innerHTML = sig_name;
-  time = today.getTime();
-
-
-  if (sig_name == 'Water') {
-    //먼저 검색해 
-    if (water.search()) {
-      //총 3번 이상 발생했다면
-      for (var i = 1; i <= 3; i++) {
-        water.dequeue();
-      }
-
-      cnt = 0;
-      shareKakaotalk(sig_name);
-    }
-    else {
-      //발생한적없다면
-      water.enqueue(time);
-    }
-  }
-  else if (sig_name != "Silence") {
-    shareKakaotalk(sig_name);
-  }
-});
-/////////////////////////////////////////////////
-
-
-
-
-
-
-var ros = new ROSLIB.Ros({
-  url: 'ws://localhost:9090'
-});
-
-var audio_topic = new ROSLIB.Topic({
-  ros: ros,
-  name: '/audio',
-  messageType: 'std_msgs/String'
-});
-
-audio_topic.subscribe(function (m) {
-  str = m.data;
-  console.log(str);
-  str = str.replace("\"", "").replace("\"", "");
-  for (var i = 0; i < 100; i++) str = str.replace("\"", "");
-  str = str.substring(1);
-  str = str.substring(1);
-  str = str.slice(0, -1);
-  str = str.slice(0, -1);
-  //JSON.parse doesn't work. Failed to fix the syntax error.
-  str = str.replace("[", "");
-  str = str.replace("]", "");
-  str = str.replace("[", "");
-  str = str.replace("]", "");
-  str = str.replace("[", "");
-  str = str.replace("]", "");
-  str = str.substring(0, str.indexOf("["));
-  str = str.slice(0, -1);
-  str = str.slice(0, -1);
-  str = str.split(",");
-  //console.log(str);
-
-  document.getElementById("first_topic_name").innerHTML = str;
-  document.getElementById("first_topic_proba").innerHTML = str[1];
-  document.getElementById("second_topic_name").innerHTML = str[2];
-  document.getElementById("second_topic_proba").innerHTML = str[3];
-  document.getElementById("third_topic_name").innerHTML = str[4];
-  document.getElementById("third_topic_proba").innerHTML = str[5];
-
-});
 
 
 function init() {
@@ -598,8 +292,6 @@ function init() {
 }
 
 function loop(timeStamp) {
-
-
 
   // Keep requesting new frames
   if (timeStamp - lastFrameTime < FRAME_MIN_TIME) { //skip the frame if the call is too early
@@ -695,10 +387,7 @@ function tryConnectWebsocket() {
   });
 
 
-
-
-
-  var odom = new ROSLIB.Topic({
+  odom = new ROSLIB.Topic({
     ros: ros,
     name: '/t265/odom/sample/',
     messageType: 'nav_msgs/Odometry'
@@ -728,58 +417,187 @@ function tryConnectWebsocket() {
     theta = euler.z;
     //   console.log(theta);
   });
+
+  hsignal = new ROSLIB.Topic({
+    ros: ros,
+    name: '/signal',
+    //name : '/bada_audio/signal',
+    messageType: 'std_msgs/String'
+  });
+
+  hsignal.subscribe(function (m) {
+  
+    sig_name = dic1[m.data];
+    console.log("NOW SIGNAL : " + sig_name);
+  
+  
+    time = today.getTime();
+    viewtime = printNow();
+  
+    if (h.dataStore.length >= 17) {
+      h.dequeue();
+    }
+  
+    if (sig_name == dic1['Water']) {
+      //먼저 검색해 
+      if (water.search()) {
+        //총 3번 이상 발생했다면
+        for (var i = 1; i <= 3; i++) {
+          water.dequeue();
+        }
+        cnt = 0;
+        shareKakaotalk(sig_name);
+        h.enqueue([sig_name, viewtime]);
+        hidx = hidx + 1;
+        PrintHistory = h.toString();
+      }
+      else {
+        //발생한적없다면
+        water.enqueue(time);
+      }
+    }
+    else if (sig_name != dic1["Silence"]) {
+  
+      shareKakaotalk(sig_name);
+      h.enqueue([sig_name, viewtime]);
+      hidx = hidx + 1;
+      PrintHistory = h.toString();
+    }
+    document.getElementById("history").innerHTML = PrintHistory;
+  
+  });
+
+  signal = new ROSLIB.Topic({
+    ros: ros,
+    name: '/signal',
+    messageType: 'std_msgs/String'
+  });
+
+  signal.subscribe(function (m) {
+    sig_name = m.data;
+
+
+    //console.log("NOW SIGNAL : "+sig_name);
+    document.getElementById("signal").innerHTML = sig_name;
+    time = today.getTime();
+
+
+    if (sig_name == 'Water') {
+      //먼저 검색해 
+      if (water.search()) {
+        //총 3번 이상 발생했다면
+        for (var i = 1; i <= 3; i++) {
+          water.dequeue();
+        }
+
+        cnt = 0;
+        shareKakaotalk(sig_name);
+      }
+      else {
+        //발생한적없다면
+        water.enqueue(time);
+      }
+    }
+    else if (sig_name != "Silence") {
+      shareKakaotalk(sig_name);
+    }
+  });
+  /////////////////////////////////////////////////
+
+  // audio_topic = new ROSLIB.Topic({
+  //   ros: ros,
+  //   name: '/audio',
+  //   messageType: 'std_msgs/String'
+  // });
+  
+  // audio_topic.subscribe(function (m) {
+  //   str = m.data;
+  //   console.log(str);
+  //   str = str.replace("\"", "").replace("\"", "");
+  //   for (var i = 0; i < 100; i++) str = str.replace("\"", "");
+  //   str = str.substring(1);
+  //   str = str.substring(1);
+  //   str = str.slice(0, -1);
+  //   str = str.slice(0, -1);
+  //   //JSON.parse doesn't work. Failed to fix the syntax error.
+  //   str = str.replace("[", "");
+  //   str = str.replace("]", "");
+  //   str = str.replace("[", "");
+  //   str = str.replace("]", "");
+  //   str = str.replace("[", "");
+  //   str = str.replace("]", "");
+  //   str = str.substring(0, str.indexOf("["));
+  //   str = str.slice(0, -1);
+  //   str = str.slice(0, -1);
+  //   str = str.split(",");
+  //   //console.log(str);
+  
+  //   document.getElementById("first_topic_name").innerHTML = str;
+  //   document.getElementById("first_topic_proba").innerHTML = str[1];
+  //   document.getElementById("second_topic_name").innerHTML = str[2];
+  //   document.getElementById("second_topic_proba").innerHTML = str[3];
+  //   document.getElementById("third_topic_name").innerHTML = str[4];
+  //   document.getElementById("third_topic_proba").innerHTML = str[5];
+  
+
+
+  audio_topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/signal',
+    // name: '/bada_audio/audio',
+    messageType: 'std_msgs/String'
+  });
+  
+  audio_topic.subscribe(function (m) {
+  
+    str = m.data;
+  
+    for (var i = 0; i < 100; i++) str = str.replace("\"", "");
+    str = str.substring(1);
+    str = str.substring(1);
+    str = str.slice(0, -1);
+    str = str.slice(0, -1);
+    for (var i = 0; i < 100; i++) str = str.replace(",", "");
+    for (var i = 0; i < 100; i++) str = str.replace("[", "");
+    str = str.split("]");
+  
+    document.getElementById("first_topic").innerHTML = str[0];
+    document.getElementById("second_topic").innerHTML = str[1];
+    document.getElementById("third_topic").innerHTML = str[2];
+    document.getElementById("fourth_topic").innerHTML = str[3];
+    document.getElementById("fifth_topic").innerHTML = str[4];
+    document.getElementById("sixth_topic").innerHTML = str[5];
+    document.getElementById("seventh_topic").innerHTML = str[6];
+    document.getElementById("eighth_topic").innerHTML = str[7];
+    document.getElementById("ninth_topic").innerHTML = str[8];
+    document.getElementById("tenth_topic").innerHTML = str[9];
+  });
+  
+
 };
 
+function writeAlarmTest() {
+  console.log('writing data to db...');
+
+  // Make a request for a user with a given ID
+  axios.put('/alarm', { alarmType: 'knock', alarmTime: Date.now() })
+    .then(function (response) {
+      // handle success
+      // console.log(response);
+      console.log(response.status);   // should be 200 (success)
 
 
+    })
+    .catch(function (error) {
+      // handle error
+      console.error(error);
+    })
+    .then(function () {
+      // always executed
+    });
 
-///////////////////////////
-var ros = new ROSLIB.Ros({
-  url: 'ws://localhost:9090'
-});
-
-function init() {
-  canvas = document.getElementById('canvas');
-  console.log(canvas);
-  ctx = canvas.getContext('2d');
-
-  // Start the first frame request
-  window.requestAnimationFrame(loop);
-  tryConnectWebsocket();
-};
-
-function loop(timeStamp) {
-  // Keep requesting new frames
-  if (timeStamp - lastFrameTime < FRAME_MIN_TIME) { //skip the frame if the call is too early
-    window.requestAnimationFrame(loop);
-    return; // return as there is nothing to do
-  }
-  lastFrameTime = timeStamp; // remember the time of the rendered frame
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  var secondsPassed = (timeStamp - oldTimeStamp) / 1000;
-  var elapsedSeconds = (timeStamp - start) / 1000;
-  oldTimeStamp = timeStamp;
-
-  //Calculate fps
-  var fps = Math.round(10 / secondsPassed) / 10;
-
-  //Draw number to the screen
-  //ctx.fillStyle = 'white';3
-  //ctx.fillRect(0, 0, 200, 100);
-  //ctx.font = '25px Arial';
-  //ctx.fillStyle = 'black';
-  //tx.fillText("FPS: " + fps, 10, 30);
-
-  draw(timeStamp);
-
-  if (elapsedSeconds > 5) {
-    return;
-  } else {
-    window.requestAnimationFrame(loop);
-  }
 }
+
 
 function draw(timestamp) {
   // if (!start) start = timestamp;
@@ -810,107 +628,7 @@ function draw(timestamp) {
   ctx.fill(); // or context.fill()*/
 }
 
-//canvas initiation
 
-/*-----------------------------------------from------------realtime_topic.js--------------------------------------------------------- */
-// Connecting to ROS
-// -----------------
-var ros = new ROSLIB.Ros();
-
-// If there is an error on the backend, an 'error' emit will be emitted.
-ros.on('error', function (error) {
-  console.log(error);
-});
-
-// Find out exactly when we made a connection.
-ros.on('connection', function () {
-  console.log('Connection made!');
-});
-
-ros.on('close', function () {
-  console.log('Connection closed.');
-});
-
-// Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:9090');
-
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var listener = new ROSLIB.Topic({
-  ros: ros,
-  name: '/listener',
-  messageType: 'std_msgs/String'
-});
-
-// Then we add a callback to be called every time a message is published on this topic.
-listener.subscribe(function (message) {
-  // console.log('Received message on ' + listener.name + ': ' + message.data);
-
-  // If desired, we can unsubscribe from the topic as well.
-  listener.unsubscribe();
-});
-
-
-
-
-var ros = new ROSLIB.Ros({
-  url: 'ws://localhost:9090'
-});
-
-var audio_topic = new ROSLIB.Topic({
-  ros: ros,
-  name: '/signal',
-  // name: '/bada_audio/audio',
-  messageType: 'std_msgs/String'
-});
-
-audio_topic.subscribe(function (m) {
-
-  str = m.data;
-
-  for (var i = 0; i < 100; i++) str = str.replace("\"", "");
-  str = str.substring(1);
-  str = str.substring(1);
-  str = str.slice(0, -1);
-  str = str.slice(0, -1);
-  for (var i = 0; i < 100; i++) str = str.replace(",", "");
-  for (var i = 0; i < 100; i++) str = str.replace("[", "");
-  str = str.split("]");
-
-  document.getElementById("first_topic").innerHTML = str[0];
-  document.getElementById("second_topic").innerHTML = str[1];
-  document.getElementById("third_topic").innerHTML = str[2];
-  document.getElementById("fourth_topic").innerHTML = str[3];
-  document.getElementById("fifth_topic").innerHTML = str[4];
-  document.getElementById("sixth_topic").innerHTML = str[5];
-  document.getElementById("seventh_topic").innerHTML = str[6];
-  document.getElementById("eighth_topic").innerHTML = str[7];
-  document.getElementById("ninth_topic").innerHTML = str[8];
-  document.getElementById("tenth_topic").innerHTML = str[9];
-});
-
-
-function writeAlarmTest() {
-  console.log('writing data to db...');
-
-  // Make a request for a user with a given ID
-  axios.put('/alarm', { alarmType: 'knock', alarmTime: Date.now() })
-    .then(function (response) {
-      // handle success
-      // console.log(response);
-      console.log(response.status);   // should be 200 (success)
-
-
-    })
-    .catch(function (error) {
-      // handle error
-      console.error(error);
-    })
-    .then(function () {
-      // always executed
-    });
-
-}
 
 function readAlarmTest() {
   console.log('reading data from db...');
